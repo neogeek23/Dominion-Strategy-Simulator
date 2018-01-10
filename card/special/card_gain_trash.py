5b84778f1c65da95a07d566cf4915640fe0fe6b1
Brad Stein <brad.stein@gmail.com>
2018-01-10 13:05:08 -0600

    Delete card_gain_trash.py

--- card/special/card_gain_trash.py
+++
@@ -1,9 +0,0 @@
-from card.special.card_trash import CardTrash
-from card.special.card_gain import CardGain
-
-
-class CardGainTrash(CardTrash, CardGain):
-	coin_gain = 0
-
-	def effect(self):
-		self.gain_card(self.trash_card_get_cost() + self.coin_gain)
