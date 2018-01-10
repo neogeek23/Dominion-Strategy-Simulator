9d24e960b9fea5412d221a7a59dbaf66e7ff0ff2
Brad Stein <brad.stein@gmail.com>
2018-01-10 13:04:38 -0600

    Delete workshop.py

--- card/named/workshop.py
+++
@@ -1,9 +0,0 @@
-from card.basic.card_action import Action
-from card.special.card_gain import CardGain
-
-
-class Workshop(Action, CardGain):
-	coin_gain = 4
-
-	def effect(self):
-		self.gain_card(self.coin_gain)
